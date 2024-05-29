5d123df82389da3208f46fbbbffcecae2c87973b
Matej-Chmel <58189701+Matej-Chmel@users.noreply.github.com>
2024-05-29 13:35:09 +0200

    Palindrome permutation

---
+++ arrays-and-strings/04-palindrome-permutation/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	goi "github.com/Matej-Chmel/go-interview"
+)
+
+func randomPermutation(s string) string {
+	runes := []rune(s)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := len(runes) - 1; i > 0; i-- {
+		j := r.Intn(i + 1)
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
+
+func getValue(r rune) (res int) {
+	res = int(r - 'A')
+
+	if res > 25 {
+		res = res - 32
+	}
+
+	return
+}
+
+func withArray(s string) bool {
+	a := make([]bool, 26)
+
+	for _, v := range s {
+		idx := getValue(v)
+
+		if idx >= 0 {
+			a[idx] = !a[idx]
+		}
+	}
+
+	odd := false
+
+	for _, v := range a {
+		if v && odd {
+			return false
+		} else if v && !odd {
+			odd = true
+		}
+	}
+
+	return true
+}
+
+func toggle(bits *int, i int) {
+	mask := 1 << i
+
+	if (*bits & mask) == 0 {
+		*bits |= mask
+	} else {
+		*bits &= ^mask
+	}
+}
+
+func withBits(s string) bool {
+	bits := new(int)
+
+	for _, v := range s {
+		val := getValue(v)
+
+		if val >= 0 {
+			toggle(bits, val)
+		}
+	}
+
+	return (*bits & (*bits - 1)) == 0
+}
+
+func main() {
+	palindromes := [...]string{
+		"A man a plan a canal Panama",
+		"Madam in Eden Im Adam",
+		"Able was I ere I saw Elba",
+		"Never odd or even",
+		"Was it a car or a cat I saw",
+		"Eva can I see bees in a cave",
+		"No lemon no melon",
+		"Do geese see God",
+		"A Santa lived as a devil at NASA",
+		"Murder for a jar of red rum",
+	}
+
+	notPalindromes := [...]string{
+		"The quick brown fox jumps over the lazy dog",
+		"Hello world this is a test",
+		"Go is a statically typed language",
+		"I enjoy solving programming challenges",
+		"Artificial intelligence is fascinating",
+		"Cloud computing is the future",
+		"This sentence is not a palindrome",
+		"OpenAI creates advanced AI models",
+		"Learning new languages is fun",
+		"The weather today is quite pleasant",
+	}
+
+	i := goi.NewInterview[string, bool]()
+
+	for _, v := range palindromes {
+		i.AddCase(randomPermutation(v), true)
+	}
+
+	for _, v := range notPalindromes {
+		i.AddCase(randomPermutation(v), false)
+	}
+
+	i.AddSolutions(withArray, withBits)
+	i.Print()
+}
